23cde949dc464079cadb9634f48ecb5f94dbac0a
Syzuna <syzuna@syzuna-programs.de>
2022-12-22 09:20:29 +0100

    general structure changes. api calls moved to thread and so on

--- load.py
+++ load.py
@@ -1,21 +1,30 @@
+import threading
+from threading import Thread
+from queue import Queue
 import logging
 import os.path
 import sys
 import tkinter as tk
 import logging
-from os import path
 from tkinter import ttk
+from typing import TYPE_CHECKING, Any, List, Dict, Mapping, MutableMapping, Optional, Tuple
 
 import myNotebook as nb
 import requests
-from config import appname
+from config import appname, config, user_agent
+from companion import CAPIData
+
+if TYPE_CHECKING:
+    def _(x: str) -> str:
+        return x
 
 this = sys.modules[__name__] 
-this.Version = "0.0.4"
-this.ApiKey = "" #Insert API key here
-this.CommanderId = "" # Insert FID from main menu here
-this.BrowserSourceUrl = tk.StringVar(master=None, value="placeholder")
-this.MissionList = [
+this.base_url : str = "https://api.cia-gaming.de/v1/thargoid-war"
+this.version : str = "0.0.5"
+this.api_key : str = "WTwt5TGJSEer7_fVkFEKqA"
+this.commander_id : str = "b3wstrz"
+this.browsersource_url : tk.StringVar = tk.StringVar(master=None, value="placeholder")
+this.mission_list : List[str] = [
  "Mission_TW_Rescue_Alert", "Mission_TW_Rescue_UnderAttack",
  "Mission_TW_Rescue_Burning","Mission_TW_PassengerEvacuation_Alert",
  "Mission_TW_PassengerEvacuation_UnderAttack", "Mission_TW_PassengerEvacuation_Burning",
@@ -24,19 +33,39 @@ this.MissionList = [
  "Mission_TW_CollectWing_UnderAttack", "Mission_TW_Collect_UnderAttack"
 ]
 
-# This could also be returned from plugin_start3()
-plugin_name = os.path.basename(os.path.dirname(__file__))
+this.last_cmdr_lookup : Any = None
 
-# A Logger is used per 'found' plugin to make it easy to include the plugin's
-# folder name in the logging output format.
-# NB: plugin_name here *must* be the plugin's folder name as per the preceding
-#     code, else the logger won't be properly set up.
+# UI elements
+    # Preferences
+this.browsersource_url_entry : nb.Entry = None
+
+    # Main Window
+this.goidkills_label : tk.Label = None
+this.refugees_label : tk.Label = None
+this.wounded_label : tk.Label = None
+this.emergencysupplies_label : tk.Label = None
+this.recoverysupplies_label : tk.Label = None
+
+# internals
+this.getcmdr_type = "GET_CMDR"
+this.recordactivity_type ="RECORD_ACTIVITY"
+
+this.shutting_down : bool = False
+
+this.session: requests.Session = requests.Session()
+this.session.headers['User-Agent'] = user_agent
+this.session.headers['Authorization'] = this.api_key
+
+this.queue : Queue = Queue()
+this.thread : Optional[threading.Thread] = None
+
+# setup logging
+plugin_name : str = os.path.basename(os.path.dirname(__file__))
+
 logger = logging.getLogger(f'{appname}.{plugin_name}')
 
-# If the Logger has handlers then it was already set up by the core code, else
-# it needs setting up here.
 if not logger.hasHandlers():
-    level = logging.INFO  # So logger.info(...) is equivalent to print()
+    level = logging.INFO
 
     logger.setLevel(level)
     logger_channel = logging.StreamHandler()
@@ -46,23 +75,40 @@ if not logger.hasHandlers():
     logger_channel.setFormatter(logger_formatter)
     logger.addHandler(logger_channel)
 
-def plugin_start3(plugin_dir):
+def plugin_start3(plugin_dir: str) -> str:
     """
     Load this plugin into EDMC
     """     
+    logger.debug('Starting worker thread...')
+    this.thread = Thread(target=worker, name='TWT worker')
+    this.thread.daemon = True
+    this.thread.start()
 
-    headers = { 'Authorization': this.ApiKey }
-    params = dict(id=this.CommanderId)
-    response = requests.get("https://api.cia-gaming.de/v1/thargoid-war/commander", params=params, headers=headers)
-    if response.status_code == requests.codes.ok:
-        cmdr = response.json()
-        this.BrowserSourceUrl.set(cmdr["commander"]["browserSourceUrl"])
+    return "[TCIA] Thargoid War Tracker v" + this.version
 
-    return "[TCIA] Thargoid War Tracker v" + this.Version
+def plugin_stop() -> None:
+    """Stop this plugin."""
 
-def plugin_prefs(parent, cmdr, is_beta):
+    logger.debug("Shutting down...")
+
+    this.shutting_down = True
+    this.queue.put(None)
+
+    this.thread.join()
+    this.thread = None
+
+    this.session.close()
+    logger.debug("Done.")
+
+def plugin_prefs(parent: tk.Tk, cmdr: str, is_beta: bool) -> tk.Frame:
     """
-    Return a TK Frame for adding to the EDMC settings dialog.
+    Plugin preferences setup hook.
+    Any tkinter UI set up *must* be within an instance of `myNotebook.Frame`,
+    which is the return value of this function.
+    :param parent: tkinter Widget to place items in.
+    :param cmdr: Name of Commander.
+    :param is_beta: Whether game beta was detected.
+    :return: An instance of `myNotebook.Frame`.
     """
 
     frame = nb.Frame(parent)
@@ -73,70 +119,66 @@ def plugin_prefs(parent, cmdr, is_beta):
     ttk.Separator(frame, orient=tk.HORIZONTAL).grid(columnspan=2, padx=10, pady=2, sticky=tk.EW)
     nb.Label(frame, text="You can use this browser source to show your statistics through OBS while streaming").grid(padx=10, columnspan=2, sticky=tk.W)
     nb.Label(frame, text="Your Browser Source URL:").grid(column=0, padx=10, sticky=tk.W, row=4)
-    EntryPlus(frame, textvariable=this.BrowserSourceUrl, state="readonly").grid(column=1, padx=10, pady=2, sticky=tk.EW, row=4)
+    this.browsersource_url_entry = nb.Entry(frame, textvariable=this.browsersource_url, state="readonly").grid(column=1, padx=10, pady=2, sticky=tk.EW, row=4)
 
     return frame
 
-def plugin_app(parent):
+def plugin_app(parent: tk.Tk) -> tk.Frame:
     """
-    Create a frame for the EDMC main window
+    Construct this plugin's main UI, if any.
+    :param parent: The tk parent to place our widgets into.
+    :return: See PLUGINS.md#display
     """
-    headers = { 'Authorization': this.ApiKey }
-    params = dict(id=this.CommanderId)
-    response = requests.get("https://api.cia-gaming.de/v1/thargoid-war/commander", params=params, headers=headers)
-    if response.status_code == requests.codes.ok:
-        cmdr = response.json()
-        #this.goidsDestroyed == cmdr["commander"]["activityStatistics"]["thargoidsDestroyed"]
-        global GoidKills
-        global Refugees
-        global Wounded
-        global EmergencySupplies
-        global RecoverySupplies
-        this.frame = tk.Frame(parent)
-        #tk.Button(this.frame, text='Update', command=update_war_data).grid(row=1, column=2, padx=8)
-        Title = tk.Label(this.frame, text=f'[TCIA] Thargoid War Tracker v{this.Version}')
-        Title.grid(row=0, column=0, sticky=tk.W)
-        GoidKills = tk.Label(this.frame, text=f'Thargoids killed: {cmdr["commander"]["activityStatistics"]["thargoidsDestroyed"]}')
-        GoidKills.grid(row=1, column=0, sticky=tk.W)
-        Refugees = tk.Label(this.frame, text=f'Refugees evacuated: {cmdr["commander"]["activityStatistics"]["refugeesEvacuated"]}')
-        Refugees.grid(row=2, column=0, sticky=tk.W)
-        Wounded = tk.Label(this.frame, text=f'Wounded rescued: {cmdr["commander"]["activityStatistics"]["woundedRescued"]}')
-        Wounded.grid(row=3, column=0, sticky=tk.W)
-        EmergencySupplies = tk.Label(this.frame, text=f'Emergency supplies delivered: {cmdr["commander"]["activityStatistics"]["emergencySuppliesDelivered"]}')
-        EmergencySupplies.grid(row=4, column=0, sticky=tk.W)
-        RecoverySupplies = tk.Label(this.frame, text=f'Recovery supplies delivered: {cmdr["commander"]["activityStatistics"]["recoverySuppliesDelivered"]}')
-        RecoverySupplies.grid(row=5, column=0, sticky=tk.W)
-        return this.frame
+    this.frame = tk.Frame(parent)
+    this.frame.bind_all("<<GetCMDR>>", update_war_data)
+    this.frame.bind_all("<<RecordedActivity>>", fetch_cmdr)
+    # tk.Button(this.frame, text='Update', command=update_war_data).grid(row=1, column=2, padx=8)
+    Title = tk.Label(
+    this.frame, text=f'[TCIA] Thargoid War Tracker v{this.version}')
+    Title.grid(row=0, column=0, sticky=tk.W)
+    this.goidkills_label = tk.Label(this.frame, text='Thargoids killed: 0')
+    this.goidkills_label.grid(row=1, column=0, sticky=tk.W)
+    this.refugees_label = tk.Label(this.frame, text='Refugees evacuated: 0')
+    this.refugees_label.grid(row=2, column=0, sticky=tk.W)
+    this.wounded_label = tk.Label(this.frame, text='Wounded rescued: 0')
+    this.wounded_label.grid(row=3, column=0, sticky=tk.W)
+    this.emergencysupplies_label = tk.Label(this.frame, text='Emergency supplies delivered: 0')
+    this.emergencysupplies_label.grid(row=4, column=0, sticky=tk.W)
+    this.recoverysupplies_label = tk.Label(this.frame, text='Recovery supplies delivered: 0')
+    this.recoverysupplies_label.grid(row=5, column=0, sticky=tk.W)
 
-def update_war_data():
-    headers = { 'Authorization': this.ApiKey }
-    params = dict(id=this.CommanderId)
-    response = requests.get("https://api.cia-gaming.de/v1/thargoid-war/commander", params=params, headers=headers)
-    if response.status_code == requests.codes.ok:
-        cmdr = response.json()
-        GoidKills.config(text=f'Thargoids killed: {cmdr["commander"]["activityStatistics"]["thargoidsDestroyed"]}')
-        Refugees.config(text=f'Refugees evacuated: {cmdr["commander"]["activityStatistics"]["refugeesEvacuated"]}')
-        Wounded.config(text=f'Wounded rescued: {cmdr["commander"]["activityStatistics"]["woundedRescued"]}')
-        EmergencySupplies.config(text=f'Emergency supplies delivered: {cmdr["commander"]["activityStatistics"]["emergencySuppliesDelivered"]}')
-        RecoverySupplies.config(text=f'Recovery supplies delivered: {cmdr["commander"]["activityStatistics"]["recoverySuppliesDelivered"]}')
+    params : Dict =  { 'id': this.commander_id }
+    this.queue.put((this.getcmdr_type, params))
 
-def journal_entry(cmdr, is_beta, system, station, entry, state):
+    return this.frame
+
+def fetch_cmdr(event=None) -> None:
+    params : Dict =  { 'id': this.commander_id }
+    this.queue.put((this.getcmdr_type, params))
+
+def update_war_data(event=None) -> None:
+        if this.last_cmdr_lookup: 
+            this.goidkills_label.config(text=f'Thargoids killed: {this.last_cmdr_lookup["commander"]["activityStatistics"]["thargoidsDestroyed"]}')
+            this.refugees_label.config(text=f'Refugees evacuated: {this.last_cmdr_lookup["commander"]["activityStatistics"]["refugeesEvacuated"]}')
+            this.wounded_label.config(text=f'Wounded rescued: {this.last_cmdr_lookup["commander"]["activityStatistics"]["woundedRescued"]}')
+            this.emergencysupplies_label.config(text=f'Emergency supplies delivered: {this.last_cmdr_lookup["commander"]["activityStatistics"]["emergencySuppliesDelivered"]}')
+            this.recoverysupplies_label.config(text=f'Recovery supplies delivered: {this.last_cmdr_lookup["commander"]["activityStatistics"]["recoverySuppliesDelivered"]}')
+            update_browser_source(event)
+
+def update_browser_source(event=None) -> None:
+        if this.last_cmdr_lookup:
+            this.browsersource_url.set(this.last_cmdr_lookup["commander"]["browserSourceUrl"])
+
+def journal_entry(cmdr: str, is_beta: bool, system: str, station: str, entry: MutableMapping[str, Any], state: Mapping[str, any]) -> None:
     if entry["event"] == "FactionKillBond":
         if entry["VictimFaction"] == "$faction_Thargoid;":
-            headers = { 'Authorization': this.ApiKey }
-            body = { "commanderId": this.CommanderId, "type": "FactionKillBond", "bondTargetFaction": entry["VictimFaction"] }
-            response = requests.post("https://api.cia-gaming.de/v1/thargoid-war/activity", json=body, headers=headers)
-            if response.status_code == 400:
-                logger.info(response.json())
-                return
-
-            update_war_data()
+            body : Dict = { "commanderId": this.commander_id, "type": "FactionKillBond", "bondTargetFaction": entry["VictimFaction"] }
+            this.queue.put((this.recordactivity_type, body))
     
     if entry["event"] == "MissionAccepted":
-        if str(entry["Name"]).startswith(tuple(this.MissionList)):
-            headers = { 'Authorization': this.ApiKey }
-            body = { 
-                "commanderId": this.CommanderId, 
+        if str(entry["Name"]).startswith(tuple(this.mission_list)):
+            body : Dict = { 
+                "commanderId": this.commander_id, 
                 "type": "MissionAccepted",
                  "missions": [
                     {
@@ -146,71 +188,72 @@ def journal_entry(cmdr, is_beta, system, station, entry, state):
                     } 
                 ]
             }
-            response = requests.post("https://api.cia-gaming.de/v1/thargoid-war/activity", json=body, headers=headers)
-            if response.status_code == 400:
-                logger.info(response.json())
-                return
-
-            update_war_data()
+            this.queue.put((this.recordactivity_type, body))
     
     if entry["event"] == "MissionCompleted":
-        if str(entry["Name"]).startswith(tuple(this.MissionList)):            
-            headers = { 'Authorization': this.ApiKey }
-            body = { 
-                "commanderId": this.CommanderId, 
+        if str(entry["Name"]).startswith(tuple(this.mission_list)):            
+            body : Dict = { 
+                "commanderId": this.commander_id, 
                 "type": "MissionCompleted",
                 "missionId": entry["MissionID"]
             }
-            response = requests.post("https://api.cia-gaming.de/v1/thargoid-war/activity", json=body, headers=headers)
-            if response.status_code == 400:
-                logger.info(response.json())
-                return
-
-            update_war_data()
+            this.queue.put((this.recordactivity_type, body))
 
     if entry["event"] == "MissionFailed":
-        if str(entry["Name"]).startswith(tuple(this.MissionList)):
-            headers = { 'Authorization': this.ApiKey }
+        if str(entry["Name"]).startswith(tuple(this.mission_list)):
             body = { 
-                "commanderId": this.CommanderId, 
+                "commanderId": this.commander_id, 
                 "type": "MissionFailed",
                 "missionId": entry["MissionID"]
             }
-            response = requests.post("https://api.cia-gaming.de/v1/thargoid-war/activity", json=body, headers=headers)        
-            if response.status_code == 400:
-                logger.info(response.json())
-                return
-
-            update_war_data()
+            this.queue.put((this.recordactivity_type, body))
 
     if entry["event"] == "MissionAbandoned":
-        if str(entry["Name"]).startswith(tuple(this.MissionList)):
-            headers = { 'Authorization': this.ApiKey }
+        if str(entry["Name"]).startswith(tuple(this.mission_list)):
             body = { 
-                "commanderId": this.CommanderId, 
+                "commanderId": this.commander_id, 
                 "type": "MissionAbandoned",
                 "missionId": entry["MissionID"]
             }
-            response = requests.post("https://api.cia-gaming.de/v1/thargoid-war/activity", json=body, headers=headers)  
-            if response.status_code == 400:
-                logger.info(response.json())
-                return
+            this.queue.put((this.recordactivity_type, body))
 
-            update_war_data()
-    
-class EntryPlus(ttk.Entry):
+def worker() -> None:
     """
-    Subclass of ttk.Entry to install a context-sensitive menu on right-click
+    Handle communication with the API.
+    Target function of a thread.
+    Processes `this.queue` until the queued item is None.
     """
-    def __init__(self, *args, **kwargs):
-        ttk.Entry.__init__(self, *args, **kwargs)
-        # overwrite default class binding so we don't need to return "break"
-        self.bind_class("Entry", "<Control-a>", self.event_select_all)
-        self.bind("<Button-3><ButtonRelease-3>", self.show_menu)
+    logger.debug('Starting worker...')
 
-    def event_select_all(self, *args):
-        self.focus_force()
-        self.selection_range(0, tk.END)
+    closing = False
 
-    def show_menu(self, e):
-        self.menu.tk_popup(e.x_root, e.y_root)
+    type : str = None
+    params: Dict = {}
+
+    while True:
+        if this.shutting_down:
+            logger.debug(f'{this.shutting_down=}, so setting closing = True')
+            closing = True
+
+        item : Optional[Tuple[str, Dict]] = this.queue.get()
+
+        if item:
+            (type, params) = item
+        else:
+            logger.debug('Empty queue message, setting closing = True')
+            closing = True
+
+        if closing:
+            logger.debug('closing, so returning.')
+            return
+
+        if type == "GET_CMDR":
+            response : requests.Response = this.session.get(f'{this.base_url}/commander', params=params)
+            response.raise_for_status()
+            this.last_cmdr_lookup = response.json()
+            this.frame.event_generate("<<GetCMDR>>", when="tail")
+
+        if type == "RECORD_ACTIVITY":
+            response : requests.Response = this.session.post(f'{this.base_url}/activity', json=params)
+            response.raise_for_status()
+            this.frame.event_generate("<<RecordedActivity>>", when="tail")
